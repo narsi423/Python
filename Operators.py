03ae13095d853770acfb458e4679f9e08b4aab0a
narsi423 <narasimhagade782@gmail.com>
2018-05-11 00:59:44 +0530

    test

---
+++ Operators.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 20
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+num3 = num1+num2
+print(num3)
+print(2**3)
+print(10%3)
+print(num2/num1)
+print(-10/2)
+print(10/-2)
+print(7//3)
+print(-10//2)
+print(10//-2)
